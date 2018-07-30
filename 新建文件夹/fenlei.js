732e5da6064988655eee1dd50920b2e1eeb8d746
LFK <1198943841@qq.com>
2018-07-30 22:42:53 +0800

    aaa

---
+++ 新建文件夹/fenlei.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+class Fenlei extends React.Component{
+	constructor(props){
+        super(props)
+   }
+   render(){
+	   return(
+	   		<div className="ele-lfk-box">
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/7/d8/a867c870b22bc74c87c348b75528djpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">美食</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/e/89/185f7259ebda19e16123884a60ef2jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">晚餐</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/c/7e/76a23eb90dada42528bc41499d6f8jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">商超便利</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/0/d0/dd7c960f08cdc756b1d3ad54978fdjpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">果蔬生鲜</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/e/8a/1a58f9145075322665b12552da1aejpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">新店特惠</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/7/0a/af108e256ebc9f02db599592ae655jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">医疗健康</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/a/7b/b02bd836411c016935d258b300cfejpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">大牌5折</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/3/01/c888acb2c8ba9e0c813f36ec9e90ajpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">浪漫鲜花</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/e/c7/b7ba9547aa700bd20d0420e1794a8jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">麻辣烫</span>
+					</a>
+					<a href="#">
+						<div className="ele-lfk-pic">
+							<img src="//fuss10.elemecdn.com/a/8a/ec21096d528b7cfd23cdd894f01c6jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" />
+						</div>
+						<span className="ele-lfk-wz">地方菜系</span>
+					</a>
+				</div>
+	   )
+   }
+}
+
+export default Fenlei;
